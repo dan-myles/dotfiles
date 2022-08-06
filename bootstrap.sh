30cd01cefe81aac8e984b0368a08666a2a247684
danlikestocode <79137382+danlikestocode@users.noreply.github.com>
2022-08-05 23:00:15 -0700

    feat: adding more functionality

--- bootstrap.sh
+++ bootstrap.sh
@@ -95,6 +95,12 @@ GREEN='\033[0;32m'
 WHITE='\033[0;97m'
 NC='\033[0m'
 
+#Checking for appropriate permissions
+clear
+echo "Current User: $(whoami)"
+[ "$UID" -eq 0 ] || exec sudo "$0" "$@"
+clear
+
 # Checking for user confirmation
 while true; do
 	read -p "This script may overwrite existing configuration files. Do you wish to continue? (y/n): " -n 1 -r -e yn
@@ -112,20 +118,70 @@ printer "${WHITE}----------------------------------------------${NC}"
 printer "${WHITE}---- danlikestocode/dotfiles Installation ----${NC}"
 printer "${WHITE}----------------------------------------------${NC}"
 printer "${WHITE}Beginning installation${NC}"
+echo
+echo
+
+# Performing general system update
+start_spinner "- Updating and upgrading system packages... (this may take a while!)"
+sudo apt-get update -y &>/dev/null
+sudo apt-get upgrade -y &>/dev/null
+stop_spinner
+printer "${GREEN}[✓] - Finished update!${NC}"
+
+#Removing packages that may be outdated...
+start_spinner "- Removing outdated packages..."
+sleep 2
+stop_spinner
+start_spinner "- Removing nvim..."
+sudo apt-get purge neovim -y &>/dev/null
+stop_spinner
+start_spinner "- Adding the offical neovim repository..."
+sudo add-apt-repository ppa:neovim-ppa/stable -y &>/dev/null
+stop_spinner
+start_spinner "- Installing the latest stable release of neovim..."
+sudo apt-get update -y &>/dev/null
+sudo apt-get install neovim -y &>/dev/null
+stop_spinner
+
+printer "${GREEN}[✓] - Finished installing outdated packages!${NC}"
 
 # Making sure dependencies are installed
-start_spinner "- Executing some command..."
-sudo apt install git
+start_spinner "- Installing dependencies..."
+sleep 2
+stop_spinner
+start_spinner "- Installing git..."
+sudo apt install git -y  &>/dev/null
+stop_spinner
+start_spinner "- Installing wget..."
+sudo apt-get install wget -y &>/dev/null
+stop_spinner
+start_spinner "- Installing curl..."
+sudo apt install curl -y &>/dev/null
 stop_spinner
 
-printer "${GREEN}[✓] - Finished hello command! ${NC}"
+printer "${GREEN}[✓] - Finished installing dependencies!${NC}"
 
-start_spinner "test2"
-sleep 5
+# Cleaning up old dotfiles directory
+start_spinner "- Cleaning up old dotfiles directory..."
+sleep 2
+stop_spinner
+start_spinner "- Deleting any existing dotfiles directory..."
+cd ${HOME}
+rm -rdf ./dotfiles &>/dev/null
 stop_spinner
 
+printer "${GREEN}[✓] - Finished cleaning up old configuration files!${NC}"
 
+# Cloning repository into new directory
+start_spinner "- Cloning dotfiles into new directory... (default directory: ~/dotfiles)"
+sleep 2
+stop_spinner
+start_spinner "- Downloading dotfiles..."
+cd ${HOME}
+git clone https://github.com/danlikestocode/dotfiles &>/dev/null
+stop_spinner
 
+printer "${GREEN}[✓] - Finished cleaning up old configuration files!${NC}"
 
 
 tput cnorm
