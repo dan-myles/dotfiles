8de4deb528d95e35595d8b84d65029bd09fcc581
danlikestocode <79137382+danlikestocode@users.noreply.github.com>
2023-03-23 16:38:56 -0700

    updating hyprland config

--- link.sh
+++ link.sh
@@ -1,12 +1,19 @@
 #!/bin/bash
 
 # Bootstrap to link to .config directory
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
 mkdir ~/.config/
-ln -s ./hypr/ ~/.config/hypr/
-ln -s ./tmux/ ~/.config/tmux/
-ln -s ./wofi/ ~/.config/wofi/
-ln -s ./dunst/ ~/.config/dunst/
-ln -s ./kitty/ ~/.config/kitty/
-ln -s ./waybar/ ~/.config/waybar/
+rm -rd ~/.config/hypr
+rm -rd ~/.config/tmux
+rm -rd ~/.config/wofi
+rm -rd ~/.config/dunst
+rm -rd ~/.config/kitty
+rm -rd ~/.config/waybar
+ln -s $SCRIPT_DIR/hypr ~/.config/hypr
+ln -s $SCRIPT_DIR/tmux ~/.config/tmux
+ln -s $SCRIPT_DIR/wofi ~/.config/wofi
+ln -s $SCRIPT_DIR/dunst ~/.config/dunst
+ln -s $SCRIPT_DIR/kitty ~/.config/kitty
+ln -s $SCRIPT_DIR/waybar ~/.config/waybar
 echo "Finished linking all directories!"
